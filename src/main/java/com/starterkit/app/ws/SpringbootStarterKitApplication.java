3869badec5f98f9ce3d38eac9efae81d10c322b7
Anindya Roy Chowdhury <anindya23@gmail.com>
2020-05-06 22:44:18 +0600

    Initialize Spring Boot Project

---
+++ src/main/java/com/starterkit/app/ws/SpringbootStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.starterkit.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterKitApplication.class, args);
+	}
+
+}
